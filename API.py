3f2b566aa69cee9e1b859330e769592aed5c6c2d
girlprog <f.galaudu@gmail.com>
2022-08-29 00:32:44 +0100

    .gitignore fix

---
+++ API.py
@@ -0,0 +1,65 @@
+import requests
+import requests
+import time
+from datetime import datetime, timedelta
+import json
+import pprint 
+import logging
+
+config_file = open('config.json')
+config = json.load(config_file)
+pp = pprint.PrettyPrinter(indent=4)
+
+api_key = config['api_key']
+server_id = config['server_id']
+
+BASE_URL = f'https://api.hetzner.cloud/v1/servers/{server_id}'
+headers = {'User-Agent': 'Mozilla/5.0', "Authorization": f"Bearer {api_key}"}
+
+def supgrade_server():
+    yesterday = (datetime.today() - timedelta(hours=0, minutes=60)).isoformat()
+    today = datetime.today().isoformat()
+    result = requests.get(f'{BASE_URL}/metrics?'
+                        f'type=cpu&'
+                        f'start={yesterday.isoformat()}&'
+                        f'end={today}&' 
+                        f'step=120', 
+                        headers=headers)
+    print(yesterday.isoformat())
+    pp.pprint(json.loads(result.content))
+
+
+def power_off_server():
+    result = requests.post(f'{BASE_URL}/actions/poweroff', headers=headers)
+    pp.pprint(json.loads(result.content))
+    return result.content
+
+
+def power_on_server():
+    result = requests.post(f'{BASE_URL}/actions/poweron', headers=headers)
+    pp.pprint(json.loads(result.content))
+    return json.loads(result.content)
+
+
+def change_server_type(new_server):
+    headers['Content-Type'] = 'application/json'
+    # upgrade_disk needds to be set to FALSE so that downgrade is possible
+    data = {"server_type": new_server,"upgrade_disk": False}
+    result = requests.post(f'{BASE_URL}/actions/change_type', headers=headers, data=json.dumps(data))
+    pp.pprint(json.loads(result.content))
+    if result.status_code == 201:
+        return json.loads(result.content)
+    else: 
+        return f"There was an error in changing server type: {result.status_code}" 
+
+
+def get_current_server_type():
+    result = requests.get(f'{BASE_URL}', headers=headers)
+    logging.info(f"Current server: {json.loads(result.content)['server']['server_type']['name']}")
+    return json.loads(result.content)['server']['server_type']['name']
+
+
+def get_all_server_types():
+    result = requests.get(f'https://api.hetzner.cloud/v1/server_types', headers=headers)
+    pp.pprint(json.loads(result.content))
+    return json.loads(result.content)
---
+++ diskWriter.py
@@ -0,0 +1,26 @@
+import logging
+import psutil
+import time
+import datetime
+
+# monitor CPU usage
+
+# with every run of the script, this file is rewritten
+def write_cpu_usage():
+    # monitor CPU usage
+    f = open("cpu_usage.csv", "w")
+    f.write(f"Number,Time,CPU,RAM\n")
+    logging.info("Writing to file started")
+    timer = 1
+    while True:
+        f.write(f"{timer}, {datetime.datetime.now()}, {str(psutil.cpu_percent())}, {str(psutil.virtual_memory().percent)} \n")
+        f.flush()
+        timer = timer + 1
+        time.sleep(2) #slee for 60 seconds
+
+
+# this is necessary with every server downgrade or upgrade
+def empty_file_contents():
+    f = open("cpu_usage.csv", "w")
+    f.write(' ')
+    f.close()
