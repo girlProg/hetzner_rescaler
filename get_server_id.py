8c4195d33e78595e9c7f869ef3b3032944d5b8a7
girlprog <f.galaudu@gmail.com>
2022-09-15 22:40:35 +0100

    get server id

--- get_server_id.py
+++ get_server_id.py
@@ -1,9 +1,10 @@
 from API import get_server_id
 
 def get_server_id_from_name():
+    print('Please enter the name of the server and press enter:')
     server_name = input()
     id = get_server_id(str(server_name))
-    print(id)
+    print(f'Server ID: {id}')
 
 
 if __name__ == '__main__':
